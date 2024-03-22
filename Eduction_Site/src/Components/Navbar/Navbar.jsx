ad51eefe38ab2e3f579f8290dc0c384a61b55a03
shiwani thagele <78925065+shiwani-thagele@users.noreply.github.com>
2024-03-22 20:32:21 +0530

    navbar completed but logo is not loaded

--- Eduction_Site/src/Components/Navbar/Navbar.jsx
+++ Eduction_Site/src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import './Navbar.css'
-import  logo from ''
-
+import logo from '../../assets/images/logo.png'
 const Navbar = () => {
   return (
-    <nav>
-        <img src="{logo}" alt="" />
+    <nav className='container'>
+        <img src="{logo}" alt="" className='logo' />
         <ul>
           <li>Home</li>
           <li>Program</li>
           <li>About us</li>
           <li>Testimonials</li>
-          <li>Contact Us</li>
+          <li> <button className='btn'>Contact Us</button></li>
+
           </ul>
     </nav>
   )
