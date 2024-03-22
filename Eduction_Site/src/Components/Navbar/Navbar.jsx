c17654e881371b6a8a2c5a250e3d2e43449a721f
shiwani thagele <78925065+shiwani-thagele@users.noreply.github.com>
2024-03-22 20:11:35 +0530

    all images

--- Eduction_Site/src/Components/Navbar/Navbar.jsx
+++ Eduction_Site/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Navbar.css'
+import  logo from ''
+
 const Navbar = () => {
   return (
-    <div>
-        <img src="" alt="" />
-    </div>
+    <nav>
+        <img src="{logo}" alt="" />
+        <ul>
+          <li>Home</li>
+          <li>Program</li>
+          <li>About us</li>
+          <li>Testimonials</li>
+          <li>Contact Us</li>
+          </ul>
+    </nav>
   )
 }
 
